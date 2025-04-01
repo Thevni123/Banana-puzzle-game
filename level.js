be96872c9eb8132d9c40576b5388b2f754679b2d
Thevni Hettiarachchi <hettiarachchithevni@gmail.com>
2025-04-01 14:21:28 +0530

    connect levels

--- level.js
+++ level.js
@@ -1,6 +1,16 @@
-function startGame(level){
-    alert(`Starting Level ${level}!`);
-    window.location.href = `gamepg.html?level=${level}`;
+function level1(){
+    alert("Starting Level 1");
+    window.location.href = "gamepg.html";
+}
+
+function level2(){
+    alert("Starting Level 2");
+    window.location.href = "gamepg2.html";
+}
+
+function level3(){
+    alert("Starting Level 3");
+    window.location.href = "gamepg3.html";
 }
 
 document.getElementById("backToMenu").addEventListener("click",() => {
